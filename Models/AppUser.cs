6c417c122f88f4a51c613ba7058c8fdc71de08ae
jgrissom <jgrissom91@gmail.com>
2019-12-15 18:21:52 -0600

    Prepare for jwt auth using identity framework
    1) Modify appsettings.json - add connection to Identity Db
    2) Add Models/AppUser class
    3) Add Models/AppIdentityDbContext class
    4) Modify startup class - add identity db context, add & configure identity

---
+++ Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Modas.Models
+{
+    public class AppUser : IdentityUser
+    {
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Modas.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
@@ -22,20 +25,47 @@ namespace Modas
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:Modas:ConnectionString"]));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:Modas"]));
             services.AddTransient<IEventRepository, EFEventRepository>();
-            // Register JWT authentication schema / configure default JWT options
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
+            // ===== Add Identity Db Context ========
+            services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:Identity"]));
+            // ===== Add Identity ========
+            services.AddIdentity<AppUser, IdentityRole>(o =>
             {
+                // commented out since we are not performing any user/role management
+                //o.User.RequireUniqueEmail = true;
+                //o.Password.RequiredLength = 6;
+                //o.Password.RequireNonAlphanumeric = true;
+                //o.Password.RequireLowercase = false;
+                //o.Password.RequireUppercase = false;
+                //o.Password.RequireDigit = true;
+                //o.Password.RequiredUniqueChars = 1;
+            }).AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            // ===== Configure Identity =======
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Events.OnRedirectToLogin = context =>
+                {
+                    // This is needed to prevent the default redirection to account/login on 404 or other
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    return Task.CompletedTask;
+                };
+            });
+            // configure application to use jwt for authentication
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options => {
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuer = false,
                     ValidateAudience = false,
                     ValidateLifetime = true,
                     ValidateIssuerSigningKey = true,
-                    //ValidIssuer = Configuration["Jwt:Issuer"],
-                    //ValidAudience = Configuration["Jwt:Issuer"],
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
                 };
             });

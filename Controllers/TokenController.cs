8bdb2428df29afdc8fa3e7b9dd5406aa601a227d
jgrissom <jgrissom91@gmail.com>
2019-12-15 18:35:44 -0600

    Token Controller - Use Identity database for user authentication

--- Controllers/TokenController.cs
+++ Controllers/TokenController.cs
@@ -1,90 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using Modas.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Modas.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]"), RequireHttps]
     public class TokenController : Controller
     {
+        private UserManager<AppUser> _userManager;
         private IConfiguration _config;
 
-        public TokenController(IConfiguration config)
+        public TokenController(UserManager<AppUser> userManager, IConfiguration config)
         {
             _config = config;
+            _userManager = userManager;
         }
 
         [HttpPost, AllowAnonymous]
-        public IActionResult CreateToken([FromBody]LoginModel login)
+        public async Task<object> RequestToken([FromBody]userModel login)
         {
+            // default response 401 Unauthorized
             IActionResult response = Unauthorized();
-            var user = Authenticate(login);
-
-            if (user != null)
+            if (ModelState.IsValid)
             {
-                var tokenString = BuildToken(user);
-                response = Ok(new { token = tokenString });
+                AppUser user = await _userManager.FindByEmailAsync(login.Username);
+                if (user != null)
+                {
+                    var result = await _userManager.CheckPasswordAsync(user, login.Password);
+                    if (result)
+                    {
+                        // Check for role
+                        if (await _userManager.IsInRoleAsync(user, _config["Jwt:Role"]))
+                        {
+                            var tokenString = BuildToken(user);
+                            response = Ok(new { token = tokenString });
+                        }
+                        else
+                        {
+                            // 403 Forbidden
+                            response = Forbid();
+                        }
+                    }
+                }
             }
-
             return response;
         }
 
-        private string BuildToken(UserModel user)
+        private string BuildToken(AppUser user)
         {
-            var claims = new[] {
-                new Claim(JwtRegisteredClaimNames.GivenName, user.FirstName),
-                new Claim(JwtRegisteredClaimNames.FamilyName, user.LastName),
-                new Claim(JwtRegisteredClaimNames.Email, user.Email),
-                new Claim(JwtRegisteredClaimNames.Birthdate, user.Birthdate.ToString("yyyy-MM-dd"))
+            var claims = new List<Claim> {
+                //new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.Id)
+                //new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                //new Claim(JwtRegisteredClaimNames.UniqueName, user.Email)
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
 
             var token = new JwtSecurityToken(
                 null, // issuer
                 null, // audience
                 claims,
-                expires: DateTime.Now.AddDays(7),
+                expires: DateTime.Now.AddDays(Int16.Parse(_config["Jwt:ValidFor"])),
                 signingCredentials: creds);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
-        private UserModel Authenticate(LoginModel login)
+        public class userModel
         {
-            UserModel user = null;
-
-            if ((login.Username).ToLower() == "john" && login.Password == "secret")
-            {
-                user = new UserModel { FirstName = "John", LastName = "Doe", Email = "john.doe@mail.com" };
-                // BirthDate is deliberately left null
-            }
-            return user;
-        }
-
-        public class LoginModel
-        {
+            [Required, EmailAddress]
             public string Username { get; set; }
+            [Required]
             public string Password { get; set; }
-        }
-
-        private class UserModel
-        {
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public string Email { get; set; }
-            public DateTime Birthdate { get; set; }
         }
     }
 }
